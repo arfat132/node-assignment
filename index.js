ee78d9477a267d29fc56b082815be7dd6dc4f222
arfat132 <arfatbegum90@gmail.com>
2022-09-04 00:07:01 +0300

    server created

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+app.all('*', (req, res) => {
+    res.send('Any Router show ')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
